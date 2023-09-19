f083387d50dc70a01258ed9a146f6dd1b17823ea
Arzymatiq <keepa3003gmail.com>
2023-09-19 14:06:05 +0600

    ./vid

---
+++ src/pages/VideosListPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideosList from "../components/videos/VideosList";
+
+const VideosListPage = () => {
+    return (
+        <div>
+            <VideosList />
+        </div>
+    );
+};
+
+export default VideosListPage;
