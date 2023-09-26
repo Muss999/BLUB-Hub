4be9437034f793e8fb81164b3e32ccc3dfad91e3
Muss999 <mussangle1@gmail.com>
2023-09-26 20:18:23 +0600

    added details

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,4 +1,5 @@
-export const API = "http://35.238.104.20/";
+// export const API = "http://35.238.104.20/";
+export const API = "http://35.222.139.123/";
 
 //! full api
 // export const API = "http://35.238.104.20//api/v1";
