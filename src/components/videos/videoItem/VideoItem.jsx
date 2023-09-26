216b073d2408eb7c42c390ba1df88096fd148997
mayyean <marzhankataeva64@gmail.com>
2023-09-26 14:39:54 +0600

    postlist

--- src/components/videos/videoItem/VideoItem.jsx
+++ src/components/videos/videoItem/VideoItem.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import "./videoItem.model.css";
+// import "./videoItem.model.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import styles from "./videoItem.module.css";
 
 const VideoItem = ({ item }) => {
-    const navigate = useNavigate();
-    return (
-        <Link to={`/video-details/${item.slug}`} className="video_item">
-            <img src={item.video_preview} alt="" width="100px" height="100px" />
-            <h3>{item.title}</h3>
-        </Link>
-    );
+  const navigate = useNavigate();
+  return (
+    <div className={styles.video_container}>
+      <div
+        onClick={() => navigate(`/video-details/${item.slug}`)}
+        className={styles.video_item}
+      >
+        <div className={styles.video_preview_container}>
+          <img src={item.video_preview} />
+        </div>
+        <div className={styles.video_item_text}>
+          <h3>{item.title}</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
